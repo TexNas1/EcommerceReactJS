3f5cdf684ac59d442aabf4bf087aad860dd82c00
texNAS <afaqhafeez0@gmail.com>
2021-12-17 00:21:26 +0500

    make it bettxxxvr

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/ElholmClothing",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://TexNAS:SignPass365@clusterec.urhsv.mongodb.net/elohim-clothing?retryWrites=true&w=majority",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || "sb",
 };
